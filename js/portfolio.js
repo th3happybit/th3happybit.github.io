bdb96db08281c4228034600277561578928e8cd2
OmarMokhfi <omar.mokhfi.official@gmail.com>
2018-12-04 06:18:19 -0800

    added creation from database

--- js/portfolio.js
+++ js/portfolio.js
@@ -17,5 +17,22 @@ firebase.database().ref('work/').once('value').then(function(snapshot) {
         console.log(article.subtitle);
         console.log(article.head);
         console.log(article.description);
+        
+        var parent = document.createElement("div");
+        parent.setAttribute("class", "col-md-4 col-sm-4 team animated slideInUp");
+        parent.setAttribute("data-tag", article.type);
+        parent.innerHTML = '<div class="team-member">' +
+        '<div class="team-img"> <img src="'+ article.img +'" alt="team member" class="h-100 w-100"> </div>' +
+        ' <div class="team-hover">'
+            + '<div class="desk">'
+                +'<h4>' + article.head + '</h4>'
+                +'<p>' + article.description +'</p>'
+            +'</div>'
+        +'</div>'
+        +'</div>'
+        +'<div class="team-title text-left">'
+        +'<h5 class="green">'+ article.title +'</h5> <span>'+ article.subtitle +'</span> </div>'
+
+        parentNode.append(parent);
     });
 });
